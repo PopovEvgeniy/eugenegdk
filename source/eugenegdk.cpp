eca90b3bfbcefe2f9bb86310edc56b45a404ed43
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-07-11 18:25:37 +0700

    Image resizer improved

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -586,8 +586,6 @@ namespace EUGENEGDK
    image.set_length(0);
    source_width=0;
    source_height=0;
-   x_ratio=0;
-   y_ratio=0;
    target_width=1;
    target_height=1;
   }
@@ -600,16 +598,6 @@ namespace EUGENEGDK
   size_t Resizer::get_source_offset(const unsigned int x,const unsigned int y) const
   {
    return static_cast<size_t>(x)+static_cast<size_t>(y)*static_cast<size_t>(source_width);
-  }
-
-  unsigned int Resizer::get_source_x(const unsigned int target_x) const
-  {
-   return (target_x*x_ratio)/UCHAR_MAX;
-  }
-
-  unsigned int Resizer::get_source_y(const unsigned int target_y) const
-  {
-   return (target_y*y_ratio)/UCHAR_MAX;
   }
 
   void Resizer::load_image(const unsigned int *target)
@@ -625,12 +613,14 @@ namespace EUGENEGDK
   void Resizer::scale_image(const unsigned int *target)
   {
    size_t index;
-   unsigned int x,y;
+   unsigned int x,y,x_ratio,y_ratio;
    x=0;
    y=0;
+   x_ratio=(source_width*UCHAR_MAX)/target_width;
+   y_ratio=(source_height*UCHAR_MAX)/target_height;
    for (index=0;index<image.get_length();++index)
    {
-    image[index]=target[this->get_source_offset(this->get_source_x(x),this->get_source_y(y))];
+    image[index]=target[this->get_source_offset((x*x_ratio)/UCHAR_MAX,(y*y_ratio)/UCHAR_MAX)];
     ++x;
     if (x==target_width)
     {
@@ -674,12 +664,6 @@ namespace EUGENEGDK
 
   }
 
-  void Resizer::calculate_scale_ratio()
-  {
-   x_ratio=(source_width*UCHAR_MAX)/target_width;
-   y_ratio=(source_height*UCHAR_MAX)/target_height;
-  }
-
   void Resizer::calculate_size()
   {
    while (target_width<source_width)
@@ -706,7 +690,6 @@ namespace EUGENEGDK
    this->set_setting(width,height);
    this->calculate_size();
    this->correct_size(limit);
-   this->calculate_scale_ratio();
    this->create_texture();
    this->resize_image(target);
   }
