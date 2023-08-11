759dec524d6e459f007aba1d5b54e85c2a9b5748
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2023-08-11 18:03:12 +0700

    Small render bug fixed

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -890,6 +890,8 @@ namespace EUGENEGDK
    glTexEnvi(GL_TEXTURE_ENV,GL_TEXTURE_ENV_MODE,GL_REPLACE);
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MAG_FILTER,GL_LINEAR);
    glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_MIN_FILTER,GL_LINEAR);
+   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_S,GL_CLAMP);
+   glTexParameteri(GL_TEXTURE_2D,GL_TEXTURE_WRAP_T,GL_CLAMP);
    glTexImage2D(GL_TEXTURE_2D,0,GL_RGBA,resizer.get_width(),resizer.get_height(),0,GL_BGRA_EXT,GL_UNSIGNED_BYTE,resizer.get_buffer());
   }
 
