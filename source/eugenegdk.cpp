992c01f2af54ce9bec9bb9e08eeedb1faeb0cf4f
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-02-05 19:02:09 +0700

    Small bug in the tile-map subsystem fixed

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -4158,22 +4158,8 @@ namespace EUGENEGDK
   EUGENEGDK::BOX Tilemap::get_box(const unsigned int row,const unsigned int column) const
   {
    EUGENEGDK::BOX collision;
-   if (row>0)
-   {
-    collision.x=this->get_x(row-1);
-   }
-   else
-   {
-    collision.x=this->get_x(0);
-   }
-   if (column>0)
-   {
-    collision.y=this->get_y(column-1);
-   }
-   else
-   {
-    collision.y=this->get_y(0);
-   }
+   collision.x=this->get_x(row);
+   collision.y=this->get_y(column);
    collision.width=cell_width;
    collision.height=cell_height;
    return collision;
