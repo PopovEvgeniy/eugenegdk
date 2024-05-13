2340282e5d22c244cb729d65ebf247aa9186c44e
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-05-13 17:17:15 +0700

    Small changes

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -414,7 +414,7 @@ namespace EUGENEGDK
    setting.cDepthBits=16;
    setting.nSize=sizeof(PIXELFORMATDESCRIPTOR);
    setting.nVersion=1;
-   setting.dwFlags=PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL|PFD_DOUBLEBUFFER;
+   setting.dwFlags=PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL|PFD_DOUBLEBUFFER|PFD_STEREO_DONTCARE;
    setting.iPixelType=PFD_TYPE_RGBA;
    setting.iLayerType=PFD_MAIN_PLANE;
   }
@@ -1117,6 +1117,7 @@ namespace EUGENEGDK
 
   void Render::set_common_setting()
   {
+   glDrawBuffer(GL_BACK);
    glShadeModel(GL_FLAT);
    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
    glAlphaFunc(GL_GREATER,0.1f);
