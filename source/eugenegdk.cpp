4343a145f865763d13d251e5d2b81852415a38bb
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-05-19 18:47:02 +0700

    Small changes

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -1118,6 +1118,7 @@ namespace EUGENEGDK
   void Render::set_common_setting()
   {
    glDrawBuffer(GL_BACK);
+   glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
    glShadeModel(GL_FLAT);
    glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
    glAlphaFunc(GL_GREATER,0.1f);
