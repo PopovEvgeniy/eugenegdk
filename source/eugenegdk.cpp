509ce6d5fc2b3569568b53683fdbfc621e7f6ca7
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-07-15 23:41:21 +0700

    Small changes

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -397,27 +397,7 @@ namespace EUGENEGDK
 
   WINGL::WINGL()
   {
-   render=NULL;
-   wglSwapIntervalEXT=NULL;
-   setting.bReserved=0;
-   setting.cAccumAlphaBits=0;
-   setting.cAccumBits=0;
-   setting.cAccumBlueBits=0;
-   setting.cAccumGreenBits=0;
-   setting.cAccumRedBits=0;
-   setting.cAlphaBits=0;
-   setting.cAlphaShift=0;
-   setting.cAuxBuffers=0;
-   setting.cBlueBits=0;
-   setting.cBlueShift=0;
-   setting.cGreenBits=0;
-   setting.cGreenShift=0;
-   setting.cRedBits=0;
-   setting.cRedShift=0;
-   setting.cStencilBits=0;
-   setting.dwDamageMask=0;
-   setting.dwLayerMask=0;
-   setting.dwVisibleMask=0;
+   memset(&setting,0,sizeof(PIXELFORMATDESCRIPTOR));
    setting.cColorBits=24;
    setting.cDepthBits=16;
    setting.nSize=sizeof(PIXELFORMATDESCRIPTOR);
@@ -425,6 +405,8 @@ namespace EUGENEGDK
    setting.dwFlags=PFD_DRAW_TO_WINDOW|PFD_SUPPORT_OPENGL|PFD_DOUBLEBUFFER|PFD_STEREO_DONTCARE;
    setting.iPixelType=PFD_TYPE_RGBA;
    setting.iLayerType=PFD_MAIN_PLANE;
+   render=NULL;
+   wglSwapIntervalEXT=NULL;
   }
 
   WINGL::~WINGL()
