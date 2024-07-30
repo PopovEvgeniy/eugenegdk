52486c555a2f5ef54c406976ab314bbbc2aca872
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2024-07-31 03:42:20 +0700

    Text drawing subsystem improved

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -3578,6 +3578,11 @@ namespace EUGENEGDK
    orientation=target;
   }
 
+  bool Text::is_font_load() const
+  {
+   return text.is_load();
+  }
+
   unsigned int Text::get_font_width() const
   {
    return text.get_width();
--- source/eugenegdk.h
+++ source/eugenegdk.h
@@ -941,6 +941,7 @@ typedef enum
    Text* get_handle();
    EUGENEGDK::TEXT_KIND get_orientation() const;
    void set_orientation(const EUGENEGDK::TEXT_KIND target);
+   bool is_font_load() const;
    unsigned int get_font_width() const;
    unsigned int get_font_height() const;
    void set_position(const unsigned int x,const unsigned int y);
