4c4b376c28468c2ccfded9a9d8f4ee7ddcaeba61
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-01-23 20:38:30 +0700

    The sprite sheet subsystem has improved

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -3159,11 +3159,7 @@ namespace EUGENEGDK
    row=1;
    if (this->check_frame(target)==true)
    {
-    row=target%rows;
-   }
-   if (row==0)
-   {
-    row=rows;
+    row+=(target-1)%rows;
    }
    return row;
   }
@@ -3172,13 +3168,9 @@ namespace EUGENEGDK
   {
    unsigned int column;
    column=1;
-   if (target>rows)
+   if (this->check_frame(target)==true)
    {
-    if (this->check_frame(target)==true)
-    {
-     column+=(target-1)/rows;
-    }
-
+    column+=(target-1)/rows;
    }
    return column;
   }
@@ -3261,7 +3253,11 @@ namespace EUGENEGDK
   void Sheet::set_target(const unsigned int target)
   {
    this->set_frame(target);
-   this->select(this->get_row(this->get_frame()),this->get_column(this->get_frame()));
+   if (this->check_frame(target)==true)
+   {
+    this->select(this->get_row(target),this->get_column(target));
+   }
+
   }
 
   void Sheet::step()
