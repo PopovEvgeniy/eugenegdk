c88512c1c6aab14bf0ca7a437edd486a82db24e2
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-01-27 21:41:53 +0700

    The resource manager has improved

--- source/eugenegdk.h
+++ source/eugenegdk.h
@@ -292,21 +292,13 @@ typedef enum
   template <class RESOURCE>
   void destroy(RESOURCE *target)
   {
-   if (target!=NULL)
-   {
-    delete target;
-   }
-
+   delete target;
   }
 
   template <class RESOURCE>
   void destroy_array(RESOURCE *target)
   {
-   if (target!=NULL)
-   {
-    delete[] target;
-   }
-
+   delete[] target;
   }
 
  }
