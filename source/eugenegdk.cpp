f89dd86b46a7eb8ce9323e1aa5be413f78650b32
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-02-04 03:31:58 +0700

    The small changes

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -1071,7 +1071,6 @@ namespace EUGENEGDK
    glDisable(GL_COLOR_LOGIC_OP);
    glDisable(GL_FOG);
    glDisable(GL_STENCIL_TEST);
-   glDisable(GL_SCISSOR_TEST);
    glDisable(GL_LIGHTING);
    glDisable(GL_NORMALIZE);
    glDisable(GL_AUTO_NORMAL);
@@ -1100,6 +1099,7 @@ namespace EUGENEGDK
    glDisable(GL_MAP2_TEXTURE_COORD_4);
    glDisable(GL_MAP2_VERTEX_3);
    glDisable(GL_MAP2_VERTEX_4);
+   glEnable(GL_SCISSOR_TEST);
    glEnable(GL_TEXTURE_2D);
    glEnable(GL_ALPHA_TEST);
    glEnable(GL_BLEND);
@@ -1151,6 +1151,7 @@ namespace EUGENEGDK
   glMatrixMode(GL_PROJECTION);
   glLoadIdentity();
   glOrtho(0.0,static_cast<double>(width),static_cast<double>(height),0.0,0.0,1.0);
+  glScissor(0,0,width,height);
   glViewport(0,0,width,height);
  }
 
