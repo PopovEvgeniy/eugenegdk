83a8a859b0f0fa3b9a4d5c4551183445857890eb
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-05 22:07:39 +0700

    The parallax background subsystem has improved

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -3971,6 +3971,21 @@ namespace EUGENEGDK
    stage.complex_mirror();
   }
 
+  bool Parallax::is_horizontally_mirrored() const
+  {
+   return stage.is_horizontally_mirrored();
+  }
+
+  bool Parallax::is_vertically_mirrored() const
+  {
+   return stage.is_vertically_mirrored();
+  }
+
+  bool Parallax::is_complex_mirrored() const
+  {
+   return stage.is_complex_mirrored();
+  }
+
   void Parallax::destroy_image()
   {
    stage.destroy_image();
