00d9bca542d3c92dcb09df01c9c3305d26c1fc5c
Popov Evgeniy Alekseyevich <tuzik87@inbox.ru>
2025-11-16 01:43:45 +0700

    The rendering performance has increased

--- source/eugenegdk.cpp
+++ source/eugenegdk.cpp
@@ -991,7 +991,6 @@ namespace EUGENEGDK
    if (glIsEnabled(GL_ALPHA_TEST)==GL_FALSE)
    {
     glEnable(GL_ALPHA_TEST);
-    glEnable(GL_BLEND);
    }
 
   }
@@ -1001,7 +1000,6 @@ namespace EUGENEGDK
    if (glIsEnabled(GL_ALPHA_TEST)==GL_TRUE)
    {
     glDisable(GL_ALPHA_TEST);
-    glDisable(GL_BLEND);
    }
 
   }
@@ -1078,6 +1076,7 @@ namespace EUGENEGDK
   void Render::set_perfomance_settings()
   {
    glDisable(GL_TEXTURE_1D);
+   glDisable(GL_BLEND);
    glDisable(GL_TEXTURE_GEN_Q);
    glDisable(GL_TEXTURE_GEN_R);
    glDisable(GL_TEXTURE_GEN_S);
@@ -1120,7 +1119,6 @@ namespace EUGENEGDK
    glDisable(GL_MAP2_VERTEX_3);
    glDisable(GL_MAP2_VERTEX_4);
    glEnable(GL_TEXTURE_2D);
-   glEnable(GL_BLEND);
    glEnable(GL_ALPHA_TEST);
    glEnableClientState(GL_VERTEX_ARRAY);
    glEnableClientState(GL_TEXTURE_COORD_ARRAY);
@@ -1144,7 +1142,6 @@ namespace EUGENEGDK
    glDrawBuffer(GL_BACK);
    glPolygonMode(GL_FRONT_AND_BACK,GL_FILL);
    glShadeModel(GL_FLAT);
-   glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
    glAlphaFunc(GL_GREATER,0.6f);
    glClearColor(0.0,0.0,0.0,0.0);
   }
